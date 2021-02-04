2808631b6d58da9ac6ef160f0c5db3a76ae04c29
Th0mas1ee <Thomass1ee@163.com>
2021-02-04 15:15:55 +0800

    Python脚本编写进阶

---
+++ python_oop_advanced/salary/select.py
@@ -0,0 +1,5 @@
+from python_oop_advanced.salary import money
+
+
+def select_money():
+    print(f'Actual amount: {money.saved_money}')
---
+++ python_oop_advanced/salary/send.py
@@ -0,0 +1,7 @@
+from python_oop_advanced.salary import money
+
+
+def send_money():
+    print("Sending money...")
+    money.saved_money += 1000
+    print("Sending finished.")
